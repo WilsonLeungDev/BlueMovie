93eb335c74d9a6bacafcf9f4f1b950c5db911593
Wilson Leung <wilsonleung0914@gmail.com>
2022-01-30 16:17:10 +0800

    Initial Commit

---
+++ BlueMovie/BlueMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueMovieApp.swift
+//  BlueMovie
+//
+//  Created by Wilson Leung on 30/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct BlueMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
